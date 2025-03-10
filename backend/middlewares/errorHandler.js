7d9c8d9376aca87c6bf2698c320e024296473a78
Indira <indimora1109@gmail.com>
2025-03-10 10:46:03 +0100

    styling index, and register page

--- backend/middlewares/errorHandler.js
+++
@@ -1,7 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({ error: 'Something went wrong!' });
-  };
-  
-  module.exports = errorHandler;
-  
